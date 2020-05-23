a2674db1e2f3e9a4ffc905a09396e0d25c62e5a8
SayfEddine <sayf-eddine.hammemi@scality.com>
2020-05-23 13:59:35 +0200

    rework common bootstrap to its minimum

--- common/bootstrap.sh
+++ common/bootstrap.sh
@@ -1,27 +1,11 @@
 #!/bin/bash
 
 # Update hosts file
-echo "[TASK 1] Update /etc/hosts file"
+echo "[TASK] Update /etc/hosts file"
 cat >>/etc/hosts<<EOF
 192.168.2.2 master.example.com master
 192.168.2.3 worker.example.com worker
 EOF
-
-
-echo "[TASK 2] Install docker container engine"
-apt-get install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update -y
-apt-get install docker-ce -y
-
-# add ccount to the docker group
-usermod -aG docker vagrant
-
-# Enable docker service
-echo "[TASK 3] Enable and start docker service"
-systemctl enable docker >/dev/null 2>&1
-systemctl start docker
 
 # Download kubectl
 wget -q --show-progress --https-only --timestamping \
@@ -30,7 +14,7 @@ chmod +x kubectl
 mv kubectl /usr/local/bin/
 
 # Add sysctl settings
-echo "[TASK 4] Add sysctl settings"
+echo "[TASK] Add sysctl settings"
 cat >>/etc/sysctl.d/kubernetes.conf<<EOF
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
@@ -38,17 +22,17 @@ EOF
 sysctl --system >/dev/null 2>&1
 
 # Disable swap
-echo "[TASK 5] Disable and turn off SWAP"
+echo "[TASK] Disable and turn off SWAP"
 sed -i '/swap/d' /etc/fstab
 swapoff -a
 
 # Enable ssh password authentication
-echo "[TASK 6] Enable ssh password authentication"
+echo "[TASK] Enable ssh password authentication"
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 systemctl restart sshd
 
 # Set Root password
-echo "[TASK 7] Set root password"
+echo "[TASK] Set root password"
 echo -e "kubeadmin\nkubeadmin" | passwd root
 #echo "kubeadmin" | passwd --stdin root >/dev/null 2>&1
 
